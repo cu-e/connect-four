935d5a42fdbf40574bbd24218fd06f053ef8e713
Egor <egor.toropov@yahoo.com>
2025-10-26 05:29:18 +0500

    init(workspaces): настроить окружение

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+import js from '@eslint/js';
+import tslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.next/**',
+      '**/.turbo/**',
+      '**/coverage/**',
+    ],
+  },
+  js.configs.recommended,
+  ...tslint.configs.recommended,
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tslint.parser,
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      import: importPlugin,
+      prettier,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'prettier/prettier': 'error',
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'import/order': [
+        'warn',
+        { 'newlines-between': 'always', alphabetize: { order: 'asc', caseInsensitive: true } },
+      ],
+    },
+  },
+
+  {
+    files: ['server/**/*.{ts,tsx,js,jsx}'],
+    languageOptions: { globals: globals.node },
+    rules: { 'no-console': 'off' },
+  },
+
+  {
+    files: ['client/**/*.{ts,tsx,js,jsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+];
